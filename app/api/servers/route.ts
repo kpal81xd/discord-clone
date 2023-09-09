95ca16e8dcf48195d5cd213899ea20fcb956ecb7
KPal <kpal81xd@gmail.com>
2023-09-09 21:44:51 +0100

    copy app

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -9,6 +9,7 @@ export async function POST(req: Request) {
   try {
     const { name, imageUrl } = await req.json();
     const profile = await currentProfile();
+
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
@@ -30,7 +31,7 @@ export async function POST(req: Request) {
 
     return NextResponse.json(server);
   } catch (error) {
-    console.log("[SERVERS POST]", error);
+    console.log("[SERVERS_POST]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
